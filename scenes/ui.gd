98ea5a40883eba0ff2eefae9b989d992bd0d8d4f
sha <nonelpublic@proton.me>
2024-10-17 00:50:43 +0200

    Added basic UI skeleton with input handling

---
+++ scenes/ui.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var opened_bible = get_node("OpenBible")
+@onready var menu = get_node("Menu")
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("menu"):
+		if opened_bible.visible:
+			opened_bible.visible = false
+			
+		elif menu.visible:
+			menu.visible = false
+		else:
+			menu.visible = true
+
+func _on_bible_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_pressed("lbm"):
+		opened_bible.visible = true
