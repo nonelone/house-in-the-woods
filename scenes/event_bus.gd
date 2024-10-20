712d99a984ff372aa99f91f6ee0367a2eec0ac9c
sha <nonelpublic@proton.me>
2024-10-20 02:10:12 +0200

    Added Universal Event Bus (UEB)

---
+++ scenes/event_bus.gd
@@ -0,0 +1,15 @@
+extends Node
+
+signal in_range(name)
+signal out_range(name)
+
+signal interact_with(name, equipement)
+
+signal push_message(message)
+signal push_popup(popup, data)
+signal close_popup
+
+signal increase_haunt(amount)
+signal decrease_haunt(amount)
+
+signal next_omen
